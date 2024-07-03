2801c073c06dfb6567afbfc881a912d78379f93d
Prince-armorcode <142484805+Prince-armorcode@users.noreply.github.com>
2024-07-03 16:40:10 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,12 +4,13 @@ resource "aws_vpc" "tfgoat" {
 
 data "aws_availability_zones" "available" {}
 
+resource "aws_subnet" "tfgoat"{
   count                   = 2
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.tfgoat.id
-
+}
 
 resource "aws_internet_gateway" "tfgoat" {
   vpc_id = aws_vpc.tfgoat.id
